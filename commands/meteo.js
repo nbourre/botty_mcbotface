279a8e6726aea15302dfe44d8a94911995d52773
Nick B <nbourrereg@hotmail.com>
2023-02-21 21:13:14 -0500

    Voir la longueur du data

--- commands/meteo.js
+++ commands/meteo.js
@@ -5,7 +5,15 @@ const axios = require('axios');
 module.exports = async function (msg, args) {
     const endpoint = "https://wttr.in/Shawinigan?T&M&lang=fr&AnF";
     const response = await axios.get(endpoint);
-    console.log(response.data);
+
+    let length = response.data.length;
+
+    console.log(`Data length : ${length}}`);
+
+    if (length > 2000) {
+        msg.reply(`Trop de données à afficher. Endpoint : ${endpoint}`);
+        return;
+    }   
 
     msg.reply(response.data); 
 }

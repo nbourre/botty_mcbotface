0f0c9537a9735cdd7e07c767430b0bba43b6cb23
Nick B <nbourrereg@hotmail.com>
2023-02-21 21:09:57 -0500

    Plus court

--- commands/meteo.js
+++ commands/meteo.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 // function that get the weather of a city using
 // axios wttr.in/Shawinigan?M&lang=fr
 module.exports = async function (msg, args) {
-    const endpoint = "https://wttr.in/Shawinigan?T&M&lang=fr&An";
+    const endpoint = "https://wttr.in/Shawinigan?T&M&lang=fr&AnF";
     const response = await axios.get(endpoint);
     console.log(response.data);
 

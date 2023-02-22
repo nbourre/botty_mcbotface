57dd558551fede1bd89b2114c67d50b7cab1c830
Nick B <nbourrereg@hotmail.com>
2023-02-21 21:21:29 -0500

    Ajout de la ville

--- commands/meteo.js
+++ commands/meteo.js
@@ -3,7 +3,14 @@ const axios = require('axios');
 // function that get the weather of a city using
 // axios wttr.in/Shawinigan?M&lang=fr
 module.exports = async function (msg, args) {
-    const endpoint = "https://wttr.in/Shawinigan?T&M&lang=fr&AnF0";
+    // Variable to store the city name from args
+    let city = args[0];
+    
+    if (!city) {       
+        city = "Shawinigan";
+    }
+
+    const endpoint = `https://wttr.in/${city}?T&M&lang=fr&AnF0`;
     const response = await axios.get(endpoint);
 
     let length = response.data.length;

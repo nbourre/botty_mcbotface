c717a057e30aaa50d550084fc5e57526333bd217
Nick B <nbourrereg@hotmail.com>
2023-02-21 21:16:21 -0500

    Plus court

--- commands/meteo.js
+++ commands/meteo.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 // function that get the weather of a city using
 // axios wttr.in/Shawinigan?M&lang=fr
 module.exports = async function (msg, args) {
-    const endpoint = "https://wttr.in/Shawinigan?T&M&lang=fr&AnF";
+    const endpoint = "https://wttr.in/Shawinigan?T&M&lang=fr&AnF0";
     const response = await axios.get(endpoint);
 
     let length = response.data.length;
@@ -11,7 +11,7 @@ module.exports = async function (msg, args) {
     console.log(`Data length : ${length}}`);
 
     if (length > 2000) {
-        msg.reply(`Trop de données à afficher. Endpoint : ${endpoint}`);
+        msg.reply(`Trop de données à afficher (${length} octets max 2000). Endpoint : ${endpoint}`);
         return;
     }   
 
